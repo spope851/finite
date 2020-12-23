448900be01b027278925f77ba92d52e536735221
spope5 <spope851@gmail.com>
2020-12-22 17:03:22 -0800

    price component and buy/sell buttons

--- front-end/src/components/player.js
+++ front-end/src/components/player.js
@@ -1,27 +1,75 @@
-import React from 'react';
+import * as React from 'react';
+import { useEffect } from 'react';
 import PlayerDetails from './playerDetails'
 
-class Player extends React.Component{
+class Player extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = {
-      details: false
+      details: false,
+      trade: false,
+      tradePrice: null
     }
   }
   render() {
+
+    // useEffect(() => {
+    //   this.setState({trade: this.state.details ? this.state.trade : false})
+    // },[this.state.details])
+
     const toggleDetails = () => {
-      this.setState({details: !this.state.details})
+      this.setState({
+        details: !this.state.details
+      })
     }
+
+    const toggleTrade = (price) => {
+      this.setState({trade: !this.state.trade})
+      this.setState({tradePrice: price})
+    }
+
     return (
-      <div  className="card col-1">
+      <div  className="card col-4">
         <div className="card-header">
-          <span onClick={(e)=>toggleDetails(e)}>{this.props.name}</span>
+          <p onClick={toggleDetails()}>{this.props.name}</p>
+          {this.state.trade && <><BuyButton/><SellButton/></>}
         </div>
         <div className="card-body">
-          {this.state.details && <PlayerDetails data={this.props.details}/>}
+          {this.state.details && <PlayerDetails trade={toggleTrade} data={this.props.details}/>}
         </div>
       </div>
     );
+  }
+}
+
+class BuyButton extends React.Component{
+  render() {
+    const buy = () => {
+      alert('bought')
+    }
+    return (
+      <button
+        onClick={e => buy()}
+      >
+        Buy
+      </button>
+    )
+  }
+}
+
+class SellButton extends React.Component{
+  render() {
+    const sell = () => {
+      alert('sold')
+    }
+    return (
+      <button
+        onClick={e => sell()}
+      >
+        Sell
+      </button>
+    )
   }
 }
 
